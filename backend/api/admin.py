b7e9ce31dd2e982d9e6eca2f4c325654b176b88b
jakefrenzel <jakef1@hotmail.com>
2025-07-18 13:44:39 -0500

    DjangoMax v1.1.6 [Added models to admin site]

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Item, Box
-# Register your models here.
+from .models import Item, Box, Product, Setting
 
+
+# Register your example models here.
 admin.site.register(Item)
 admin.site.register(Box)
+admin.site.register(Product)
+
+
+# Register official models
+admin.site.register(Setting)
