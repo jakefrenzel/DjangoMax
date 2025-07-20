e5017ee56ba4cc501f4e7bdd57d74b33337b328b
jakefrenzel <jakef1@hotmail.com>
2025-07-20 10:44:02 -0500

    DjangoMax v1.1.20 [Added membership tiers model]

--- backend/api/models.py
+++ backend/api/models.py
@@ -88,6 +88,18 @@ class User(models.Model):
         db_table = 'users'
 
 
+class MembershipTier(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    facility_id = models.ForeignKey('Facility', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Membership Tier: {self.title} for Facility: {self.facility.id}"
+
+    class Meta:
+        db_table = 'membership_tiers'
 
 
 """
