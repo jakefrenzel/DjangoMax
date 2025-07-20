c2408227dfb1a71ce0d694bdab8f560fa1550252
jakefrenzel <jakef1@hotmail.com>
2025-07-20 11:19:04 -0500

    DjangoMax v1.1.29 [Added booking to admin site]

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Box, Product, Setting, Facility, Franchisee, User, MembershipTier, Membership, Court
+from .models import Item, Box, Product, Setting, Facility, Franchisee, User, MembershipTier, Membership, Court, Booking
 
 
 # Register your example models here.
@@ -16,3 +16,4 @@ admin.site.register(User)
 admin.site.register(MembershipTier)
 admin.site.register(Membership)
 admin.site.register(Court)
+admin.site.register(Booking)
